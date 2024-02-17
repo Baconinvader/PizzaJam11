811263dc7ab4722393b1191f64e208564473ae44
shepherd.sam@live.co.uk <shepherd.sam@live.co.uk>
2024-02-17 20:18:13 +0000

    Chicken remove fix

--- entities/Player.gd
+++ entities/Player.gd
@@ -3,12 +3,13 @@ extends "res://entities/Entity.gd"
 class_name Player
 
 var mouse_sensitivity:float = 1.0
-@export var acceleration:float = 0.1
-@export var vel_keep:float = 0.98
-@export var vel_max:float = 5.0
+@export var acceleration:float = 6.2
+@export var speed:float = 20.0
+@export var deceleration:float = 1
+@export var vel_max:float = 20.0
 
-@export var gravity:float = 0.98
-@export var jump_power:float = 5.0
+@export var gravity:float = 9.8*2
+@export var jump_power:float = 15.0
 
 func _process(delta):
 	var move_vec:Vector3 = Vector3.ZERO
@@ -24,16 +25,30 @@ func _process(delta):
 		
 	var rotate_basis:Transform3D = transform
 	rotate_basis.origin = Vector3.ZERO
-	move_vec = rotate_basis * move_vec
+	
+	var do_accel:bool = false
+	if do_accel:
+		move_vec = rotate_basis * acceleration * move_vec
+	else:
+		move_vec  = rotate_basis * speed * move_vec 
 	
 	var vert:float = velocity.y
 	velocity.y = 0.0
 	
 	#clamp horizontal
-	velocity += move_vec * delta
-	velocity *= vel_keep
-	velocity = velocity.limit_length(vel_max)
-	
+	var hor_vel:Vector2 = Vector2(velocity.x, velocity.z)
+	if ( Vector2(move_vec.x, move_vec.z).length() == 0):
+		var mag:float = min(hor_vel.length(), deceleration)
+		velocity += -(velocity.normalized()) * mag
+		print("- %s (%s, %s) %s" % [(-(velocity.normalized()) * mag), mag, deceleration, hor_vel])
+		
+	if do_accel:
+		velocity += move_vec * delta
+		velocity = velocity.limit_length(vel_max)
+	else:
+		if move_vec.length():
+			velocity = move_vec
+		
 	if is_on_floor():
 		pass
 	else:
@@ -47,6 +62,6 @@ func jump():
 	velocity.y = jump_power
 		
 func _input(ev:InputEvent):
-	if (ev.is_action_pressed("jump")):
+	if (ev.is_action_pressed("jump") and is_on_floor() ):
 		jump()
 	
