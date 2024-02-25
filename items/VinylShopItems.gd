596e251702907ba78e250f82ac901bef24b54ed5
shepherd.sam@live.co.uk <shepherd.sam@live.co.uk>
2024-02-25 03:49:10 +0000

    Basic win screen

--- items/VinylShopItems.gd
+++ items/VinylShopItems.gd
@@ -3,6 +3,7 @@ extends Node
 class_name VinylShopItems
 
 signal item_bought
+signal all_items_bought
 
 func _ready():
 	for item:ShopItem in get_children():
@@ -10,3 +11,11 @@ func _ready():
 		
 func _on_bought_item():
 	emit_signal("item_bought")
+	var bought_all:bool = true
+	for child in get_children():
+		if not child.bought:
+			bought_all = false
+			break
+	if bought_all:
+		emit_signal("all_items_bought")
+			
